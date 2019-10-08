865051da07dd518221f4e36370b289d41ecff352
Maeva BERTRAND <mbertrand@forfaitrennes>
2019-10-08 17:48:08 +0200

    firebase signin/up/out

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bookshelves';
+  constructor() {
+    let firebaseConfig = {
+      apiKey: "AIzaSyCdkjTUV38zRuB4cYp-eQDfh-IAEWeCD2I",
+      authDomain: "bookshelves-oc-tuto.firebaseapp.com",
+      databaseURL: "https://bookshelves-oc-tuto.firebaseio.com",
+      projectId: "bookshelves-oc-tuto",
+      storageBucket: "",
+      messagingSenderId: "692882087168",
+      appId: "1:692882087168:web:50360df8ba6186b84de7e5",
+      measurementId: "G-YC0VEWMSL2"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+  }
 }
